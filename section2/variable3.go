38e09185f036226f328731689b58aa5f49e262cc
김건우 <pigonhairtt@gmail.com>
2022-01-28 17:33:12 +0900

    2022-01-28 Go Study
    2022-01-28 Go Study

---
+++ section2/variable3.go
@@ -0,0 +1,12 @@
+//변수3
+package main
+
+func main() {
+	//짧은 선언
+	//함수 안에서만 사용(전역X) , 선언 후 할당 예외 발생
+	//주로 제한된 범위의 함수내에서 사용 할 경우 코드 가독성을 높일 수 있다.  ->  추천
+
+	shortVar1 := 3
+	shortVar2 := "Test"
+	shortVar3 := false
+}
